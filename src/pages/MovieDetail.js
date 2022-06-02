195288b0e394c8aee8ec6d42b922d85c3ac37aa7
fatisenko <fatihsenko@gmail.com>
2022-06-02 13:31:16 +0300

    2.0

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -6,8 +6,8 @@ const MovieDetail = () => {
   const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState();
 
-  const API_KEY = process.env.REACT_APP_TMDB_KEY;
-  // const API_KEY = "d6278b3dc3e6f8f8376a89851c3f8c8f";
+  // const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const API_KEY = "d6278b3dc3e6f8f8376a89851c3f8c8f";
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
   const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
